078da8d68f28b9205e7e5cfd27ef48fc5c8a2fea
sandheather <heather.e.sanders22@gmail.com>
2016-02-26 17:03:20 -0500

    Markbot spacing and validation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 var i;
 var letter = prompt('Enter a Letter');
 
- for (i = 0; i < 10; i++) {
- 	document.write(letter);
- 	document.write('<br>');
- }
-
-
+for (i = 0; i < 10; i++) {
+  document.write(letter);
+  document.write('<br>');
+}
